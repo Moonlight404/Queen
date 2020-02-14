18d5d01c1c93d88811a16f9a5e4a2ee3e34b90ac
TARCISIO NUNES <tarcnux@gmail.com>
2020-02-14 02:08:34 -0200

    Fix

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
 function navigate(){
     $(".nav-link").click(function(){
     const link = "./"+$(this).data("link")+".html";
+    const name = $(this).data("link");
+    const title = $(this).html();
     if(link){
     axios.get(link)
     .then(function (response) {
         // handle success  
         $("body").html(response.data);
+        const stateObj = { foo: "bar" };
+        history.pushState(stateObj, "Queen - " + name, link);
+        document.title= "Queen - "+ title;
     })
     }
     })
